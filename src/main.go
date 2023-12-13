8dc103d63b78eabb469861e517e84ff888589298
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-12-13 23:35:29 +0530

    created some structs and rote fucntions

--- src/main.go
+++ src/main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+// step 4 Creating server server interface
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Server(rw http.ResponseWriter, r *http.Request)
+}
+
+// Step 1 created struct
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+}
+
+// Created a function for handdling the error
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println("ERR")
+		os.Exit(1)
+	}
+}
+
+// Step 3 creating a struct for loadbalancer
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int
+	servers         []Server
+}
+
+// Step 2 intialising the server
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+	return &simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
